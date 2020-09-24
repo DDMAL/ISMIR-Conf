4ebad9a83bfed81c17e7747f0c478d015fac9d30
evansavage <esav678@gmail.com>
2020-09-24 14:16:04 -0400

    Fixed music.csv to include session number

--- scripts/convert_music_table.py
+++ scripts/convert_music_table.py
@@ -21,8 +21,15 @@ new_csv = pd.DataFrame(
     "abstract": orig_csv["Abstract of the piece. This abstract is going to be displayed on the ISMIR music program page. (150-250 words)"],
     "first_name": orig_csv["Given name"],
     "last_name": orig_csv["Family name"],
+    "affiliation": orig_csv["University, Corporate / Institutional affiliation(s)"],
     "bio": orig_csv["Short bio (100-200 words)"],
     "web_link": orig_csv["Link to your website (optional)"],
+    "session": orig_csv["Session Number"],
+    "yt_link": orig_csv["YouTube link"],
+    "bb_link": orig_csv["Bilibili link"],
+    "db_link": orig_csv["Dropbox link"],
+    "still_image": orig_csv["Still image (for audio-only submissions). "],
+    
 })
 
 print(new_csv)
