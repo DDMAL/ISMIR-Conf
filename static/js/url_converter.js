c7a0132bca3fb0d0c0869e730f80e969a9f6654c
evansavage <esav678@gmail.com>
2020-09-29 13:25:34 -0400

    URL embed link extraction for youtube

---
+++ static/js/url_converter.js
@@ -0,0 +1,10 @@
+function getId(url) {
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+        return match[2];
+    } else {
+        return 'error';
+    }
+}
