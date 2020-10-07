0efe8fbc21458e0dfa8a82c7e9de7e0e4d70e08f
evansavage <esav678@gmail.com>
2020-10-06 20:57:15 -0400

    Added poster tab to the papers, made fullscreen logic more robust

--- static/js/poster.js
+++ static/js/poster.js
@@ -8,9 +8,12 @@ $('.poster-buttons button').on('click', function() {
   }
 });
 
-var button = document.querySelector('#fullscreen-button');
-
-button.addEventListener('click', fullscreen);
+$('.fullscreen-button').on('click', function() {
+  fullscreen($(this).next('.fullscreen-iframe')[0])
+})
+// var button = document.querySelector('#fullscreen-button');
+//
+// button.addEventListener('click', fullscreen);
 // when you are in fullscreen, ESC and F11 may not be trigger by keydown listener.
 // so don't use it to detect exit fullscreen
 document.addEventListener('keydown', function (e) {
@@ -22,7 +25,7 @@ document.addEventListener('mozfullscreenchange', fullscreenChange);
 document.addEventListener('fullscreenchange', fullscreenChange);
 document.addEventListener('MSFullscreenChange', fullscreenChange);
 
-function fullscreen() {
+function fullscreen(frame) {
   // check if fullscreen mode is available
   if (document.fullscreenEnabled ||
     document.webkitFullscreenEnabled ||
@@ -30,16 +33,16 @@ function fullscreen() {
     document.msFullscreenEnabled) {
 
     // which element will be fullscreen
-    var iframe = document.querySelector('#fullscreen-iframe');
+    // var iframe = document.querySelector('#fullscreen-iframe');
     // Do fullscreen
-    if (iframe.requestFullscreen) {
-      iframe.requestFullscreen();
-    } else if (iframe.webkitRequestFullscreen) {
-      iframe.webkitRequestFullscreen();
-    } else if (iframe.mozRequestFullScreen) {
-      iframe.mozRequestFullScreen();
-    } else if (iframe.msRequestFullscreen) {
-      iframe.msRequestFullscreen();
+    if (frame.requestFullscreen) {
+      frame.requestFullscreen();
+    } else if (frame.webkitRequestFullscreen) {
+      frame.webkitRequestFullscreen();
+    } else if (frame.mozRequestFullScreen) {
+      frame.mozRequestFullScreen();
+    } else if (frame.msRequestFullscreen) {
+      frame.msRequestFullscreen();
     }
   }
   else {
