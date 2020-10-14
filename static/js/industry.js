8cf63d8f76b84cc9a50a55ec68fe7f50f2773d4a
evansavage <esav678@gmail.com>
2020-10-13 23:33:45 -0400

    Added industry logos

--- static/js/industry.js
+++ static/js/industry.js
@@ -137,7 +137,8 @@ d3.selectAll('.filter_option input').on('click', function () {
 const card_html = openreview => {
     return `
       <div class="industry-card m-4 text-center">
-      <a href="industry_${openreview.UID}.html" class="image-wrapper mb-3">
+      <a href="industry_${openreview.UID}.html" class="image-wrapper mb-3" style="display: flex; align-items: center; height: 70px">
+        <img style="max-width: 100%;" src="static/images/${openreview.logo_name}" />
       </a>
       <h3><a href="industry_${openreview.UID}.html">${openreview.title}</a></h3>
       <h4>${openreview.company}</h4>
@@ -206,4 +207,3 @@ function fullscreenChange() {
 
 
 $("#fullscreen-iframe button#presentationMode").css('display', 'none !important');
-
